780bc97713f854759f3db6aca0da82cd1abf128c
Alexandr Zherdev <rndm4n@outlook.com>
2018-08-27 17:35:56 +0600

    Create table Comments

--- app.rb
+++ app.rb
@@ -25,6 +25,14 @@ configure do
 		created_date DATE,
 		content TEXT
 	)'
+
+	 @db.execute 'CREATE TABLE IF NOT EXISTS Comments
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT,
+		post_id INTEGER
+	)'
 end
 
 #Get-запросы
