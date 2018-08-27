74a114f86ace7955aca750a0c7b32187101da719
Alexandr Zherdev <rndm4n@outlook.com>
2018-08-27 17:18:11 +0600

    Add details view

--- app.rb
+++ app.rb
@@ -1,22 +1,25 @@
 #encoding: utf-8
+
+#REQUIRES:
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+#Функции, before and configure
 def init_db
-	@db = SQLite3::Database.new 'leprosorium.db'
-	@db.results_as_hash = true
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash = true
 end
 
 before do
-	init_db
+  init_db
 end
 
 configure do
-	init_db
+  init_db
 
-	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
 	(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		created_date DATE,
@@ -24,11 +27,9 @@ configure do
 	)'
 end
 
-#Get
+#Get-запросы
 get '/' do
-
-	@results = @db.execute 'SELECT * FROM Posts ORDER BY id DESC'
-
+  @results = @db.execute 'SELECT * FROM Posts ORDER BY id DESC'
   erb :index
 end
 
@@ -40,22 +41,28 @@ get '/posts' do
   erb :posts
 end
 
+#Вывод информации о посте
 get '/details/:post_id' do
-	post_id = params[:post_id]
-	erb "Displaying information for post with id #{post_id}"
+  post_id = params[:post_id]
+
+  results = @db.execute 'SELECT * FROM Posts WHERE id = ?', [post_id]
+  @row = results[0]
+
+  erb :details
 end
 
-#POST
+#POST-запросы
 
 post '/new' do
+  #Получаем переменную из POST-запроса
   @content = params[:content]
 
   if @content.length <= 0
-  	@error = 'Type post text'
-  	return erb :new
+    @error = 'Type post text'
+    return erb :new
   end
-
+  #Сохранение данных в БД
   @db.execute 'INSERT INTO Posts (content, created_date) VALUES (?, datetime())', [@content]
+  #Перенаправление на главную страницу
   redirect to '/'
-  erb  "You typed #{@content}"
 end
